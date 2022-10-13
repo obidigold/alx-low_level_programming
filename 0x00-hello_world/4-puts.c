3a3bc875c97be1fe43cef1ac0dd6686d227942f7
obidigold <obidigold@gmail.com>
2022-10-13 12:13:42 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-*main- Serves as the entry point for the program
+*main - Serves as the entry point for the program
 *
-*Return: By default returns zero if no errors otherwise returns
-non-zero value.
+*Return: Always 0 (success)
 */
 int main(void)
 {
