3cfabf8b20324f5f1b385a803a3d23503b85dd64
obidigold <obidigold@gmail.com>
2022-10-16 14:42:55 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphebet.c
+++ 0x01-variables_if_else_while/2-print_alphebet.c
@@ -3,16 +3,16 @@
  * main - program entry point.
  *
  * Return: 0 if no error, non zero if error.
- **/
+ */
 
 int main(void)
 {
 	char i;
-
+	
 	for (i = 'a'; i <= 'z'; i++);
 	{
 	putchar(i);
-	}
 	putchar('\n');
+	}
 	return (0);
 }
