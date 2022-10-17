639b0a896682141d1266d623127f457c5e1f2834
obidigold <obidigold@gmail.com>
2022-10-17 05:50:11 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
- * main-program entry point.
+ **main-program entry point.
+ *
  * Return:0 if no error, non zero if error.
- **/
+ *
+ */
 
 int main(void)
-
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
+
 	{
 		if (i == 'e' || i == 'q')
 			continue;
 		putchar(i);
-	}	putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
