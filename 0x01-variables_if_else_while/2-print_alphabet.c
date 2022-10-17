b091d839e11652af02dd06011a1f85c04f1950d5
obidigold <obidigold@gmail.com>
2022-10-17 08:58:36 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - program entry point.
- * Return: 0
+ * main - Entry point
+ * Return: Always 0
  */
-
 int main(void)
 {
-	char i;
+	char low;
 
-	for (i = 'a'; i <= 'z'; i++);
-{
-	putchar(i);
-}
-	putchar('\n');
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	putchar ('\n');
 	return (0);
-	}
+}
