d24056d993e4564e21f80812b9faf13375ce3282
obidigold <obidigold@gmail.com>
2022-10-18 12:54:23 -0700

    Task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -11,5 +12,5 @@ void print_alphabet_x10(void)
 	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('\n')
-}
+	_putchar('/n');
+	}
