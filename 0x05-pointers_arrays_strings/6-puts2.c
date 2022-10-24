56d3030219983221cc23b8533a2b524952218e2b
obidigold <obidigold@gmail.com>
2022-10-24 07:52:59 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#inlude "main.h"
+#include "main.h"
 
 /**
  * puts2 - Prints every other character of a string.
@@ -8,13 +8,18 @@
 
 void puts2(char *str)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (str[i++])
+	{
 		j++;
+	}
 
 	for (i = 0; i < j; i += 2)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
