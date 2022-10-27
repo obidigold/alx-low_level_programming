2921a4a8b1eb8bd7e17ddc48c5fa0ce854bdda38
obidigold <obidigold@gmail.com>
2022-10-27 05:00:49 -0700

    9. Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,19 @@
 
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	x = n;
 
-	_putchar((n % 10) + '0');
+	if (n / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 
 }
