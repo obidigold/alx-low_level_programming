100482f2673ef44a27fd3a71c10851bfe958a6a7
obidigold <obidigold@gmail.com>
2022-11-02 05:49:21 -0700

    2. Dreams feel real while we're in them.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,8 +3,8 @@
 /**
  * _strlen_recursion - Returns Length of String
  * @s: string
- * eturn: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
 
 int _strlen_recursion(char *s)
